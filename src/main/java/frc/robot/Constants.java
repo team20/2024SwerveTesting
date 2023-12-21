0e12bc719e3fa70c7b060220a4d281767b065610
IvyCactus <47390086+IvyCactus@users.noreply.github.com>
2023-12-21 11:47:58 -0500

    constants changing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -54,9 +54,9 @@ public final class Constants {
 		public static final double kPeakCurrentLimit = 65;
 		public static final int kPeakCurrentDurationMillis = 0;
 
-		public static final double kP = 0.001;
+		public static final double kP = 0.0005;
 		public static final double kI = 0;
 		public static final double kIz = 1;
-		public static final double kD = 0;
+		public static final double kD = 0.0000;
 	}
 }
