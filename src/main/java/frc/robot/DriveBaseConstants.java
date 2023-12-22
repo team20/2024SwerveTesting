0eb75ba1a9a5a6ca0d34ed270dbbce8779e4a335
Gold856 <117957790+Gold856@users.noreply.github.com>
2023-12-21 21:21:35 -0500

    Update constants

--- src/main/java/frc/robot/DriveBaseConstants.java
+++ src/main/java/frc/robot/DriveBaseConstants.java
@@ -4,37 +4,37 @@ import edu.wpi.first.math.geometry.Translation2d;
 
 public abstract class DriveBaseConstants {
 	public int kFLDriveID;
-	public int kBLDriveID;
 	public int kFRDriveID;
+	public int kBLDriveID;
 	public int kBRDriveID;
 
 	public int kFLSteerID;
-	public int kBLSteerID;
 	public int kFRSteerID;
+	public int kBLSteerID;
 	public int kBRSteerID;
 
 	public int kFLEncoderID;
-	public int kBLEncoderID;
 	public int kFREncoderID;
+	public int kBLEncoderID;
 	public int kBREncoderID;
 
 	public double kFLModuleOffsetRotations;
-	public double kBLModuleOffsetRotations;
 	public double kFRModuleOffsetRotations;
+	public double kBLModuleOffsetRotations;
 	public double kBRModuleOffsetRotations;
 
 	public Translation2d kFLModulePosition;
-	public Translation2d kBLModulePosition;
 	public Translation2d kFRModulePosition;
+	public Translation2d kBLModulePosition;
 	public Translation2d kBRModulePosition;
 
 	public boolean kFLSteerInvert = false;
-	public boolean kBLSteerInvert = false;
 	public boolean kFRSteerInvert = false;
+	public boolean kBLSteerInvert = false;
 	public boolean kBRSteerInvert = false;
 
 	public boolean kFLDriveInvert = false;
-	public boolean kBLDriveInvert = false;
 	public boolean kFRDriveInvert = false;
+	public boolean kBLDriveInvert = false;
 	public boolean kBRDriveInvert = false;
 }
--- src/main/java/frc/robot/LBoardConstants.java
+++ src/main/java/frc/robot/LBoardConstants.java
@@ -5,28 +5,28 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class LBoardConstants extends DriveBaseConstants {
 	public LBoardConstants() {
 		kFLDriveID = 1;
-		kBLDriveID = 7;
 		kFRDriveID = 3;
+		kBLDriveID = 7;
 		kBRDriveID = 5;
 
 		kFLSteerID = 2;
-		kBLSteerID = 8;
 		kFRSteerID = 4;
+		kBLSteerID = 8;
 		kBRSteerID = 6;
 
 		kFLEncoderID = 9;
-		kBLEncoderID = 12;
 		kFREncoderID = 10;
+		kBLEncoderID = 12;
 		kBREncoderID = 11;
 
 		kFLModuleOffsetRotations = -0.139648;
 		kFRModuleOffsetRotations = -0.336182;
 		kBLModuleOffsetRotations = 0.262451;
 		kBRModuleOffsetRotations = -0.376709;
-
-		kFLModulePosition = new Translation2d(0, 0);
-		kBLModulePosition = new Translation2d(0, 0);
-		kFRModulePosition = new Translation2d(0, 0);
-		kBRModulePosition = new Translation2d(0, 0);
+		// Centimeters
+		kFLModulePosition = new Translation2d(-29.25, 29.25);
+		kFRModulePosition = new Translation2d(29.25, 29.25);
+		kBLModulePosition = new Translation2d(-29.25, -29.25);
+		kBRModulePosition = new Translation2d(29.25, -29.25);
 	}
 }
